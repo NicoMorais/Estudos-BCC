7010c3e0991419d9a60c4a8ed6d15db8e5657d1e
NicoMorais <nicolas.fir86@gmail.com>
2025-11-06 17:47:33 -0300

    Insertion Sort em C aplicado com sucesso

--- Estudos BCC/exercicioDois.c
+++ Estudos BCC/exercicioDois.c
@@ -3,19 +3,6 @@
 #include <stdbool.h>
 #include <time.h>
 
-void preencherVetor(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 1001; // gera números aleatórios de 0 a 99
-    }
-
-}
-
-void swap(int* a, int* b) {
-    int aux = *a;
-    *a = *b;
-    *b = aux;
-}
-
 void imprimirArray(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
@@ -23,35 +10,40 @@ void imprimirArray(int arr[], int n) {
     printf("\n");
 }
 
-void bubbleSort(int arr[], int n) {
-    int temp;
-    bool swapped;
-    for (int i = 0; i < n - 1; i++) {
-        swapped = false;
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(&arr[j], &arr[j + 1]);
-                swapped = true;
-                imprimirArray(arr, n); // Imprime o array após cada troca
-            }
+void preencherVetor(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100; // gera números aleatórios de 0 a 99
+    }
+
+}
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
         }
-        if (!swapped) {
-            break; // O array já está ordenado
-        }
+        arr[j + 1] = key;
+		imprimirArray(arr, n); // Imprime o array após cada inserção
     }
 }
+
 
 int main() {
     srand(time(NULL)); // inicializa a seed para gerar valores diferentes a cada execução
 
-    int n = 10;
-    int numeros[10];
+    int n = 5;
+    int numeros[5];
 
     preencherVetor(numeros, n);
 
-    imprimirArray(numeros, n);
+    //imprimirArray(numeros, n);
 
-    bubbleSort(numeros, n);
+    insertionSort(numeros, n);
 
     return 0;
 }
